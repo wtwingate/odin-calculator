a43378fdf9c1c1ea0556584645658ab483114e94
William Wingate <wtwingate@gmail.com>
2023-11-20 22:44:59 -0500

    Add events to number and operator buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function add(x, y) {
   return x + y;
 }
@@ -27,6 +29,7 @@ function operate(num1, num2, op) {
   }
 }
 
+const display = document.querySelector("#display");
 const btn0 = document.querySelector("#num0");
 const btn1 = document.querySelector("#num1");
 const btn2 = document.querySelector("#num2");
@@ -46,3 +49,85 @@ const btnDivide = document.querySelector("#divide");
 const btnClear = document.querySelector("#clear");
 const btnToggle = document.querySelector("#toggle");
 const btnPercent = document.querySelector("#percent");
+
+let calculator = {
+  history: [],
+  number: "",
+  operator: "",
+};
+
+// Number events
+btn0.addEventListener("click", () => {
+  calculator.number += "0";
+  display.textContent = calculator.number;
+});
+
+btn1.addEventListener("click", () => {
+  calculator.number += "1";
+  display.textContent = calculator.number;
+});
+
+btn2.addEventListener("click", () => {
+  calculator.number += "2";
+  display.textContent = calculator.number;
+});
+
+btn3.addEventListener("click", () => {
+  calculator.number += "3";
+  display.textContent = calculator.number;
+});
+
+btn4.addEventListener("click", () => {
+  calculator.number += "4";
+  display.textContent = calculator.number;
+});
+
+btn5.addEventListener("click", () => {
+  calculator.number += "5";
+  display.textContent = calculator.number;
+});
+
+btn6.addEventListener("click", () => {
+  calculator.number += "6";
+  display.textContent = calculator.number;
+});
+
+btn7.addEventListener("click", () => {
+  calculator.number += "7";
+  display.textContent = calculator.number;
+});
+
+btn8.addEventListener("click", () => {
+  calculator.number += "8";
+  display.textContent = calculator.number;
+});
+
+btn9.addEventListener("click", () => {
+  calculator.number += "9";
+  display.textContent = calculator.number;
+});
+
+// Operator events
+btnAdd.addEventListener("click", () => {
+    calculator.history.push(calculator.number);
+    calculator.number = "";
+    calculator.operator = "+";
+})
+
+btnSubtract.addEventListener("click", () => {
+    calculator.history.push(calculator.number);
+    calculator.number = "";
+    calculator.operator = "-";
+})
+
+btnMultiply.addEventListener("click", () => {
+    calculator.history.push(calculator.number);
+    calculator.number = "";
+    calculator.operator = "*";
+})
+
+btnDivide.addEventListener("click", () => {
+    calculator.history.push(calculator.number);
+    calculator.number = "";
+    calculator.operator = "/";
+})
