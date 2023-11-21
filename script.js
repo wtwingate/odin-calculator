7b564cc13ebc56b32248b19081ff76ceff908a36
William Wingate <wtwingate@gmail.com>
2023-11-20 21:41:34 -0500

    Create operate function

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 function subtract(x, y) {
-    return x - y;
+  return x - y;
 }
 
 function multiply(x, y) {
-    return x * y;
+  return x * y;
 }
 
 function divide(x, y) {
-    return x / y;
+  return x / y;
+}
+
+function operate(num1, num2, op) {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
 }
